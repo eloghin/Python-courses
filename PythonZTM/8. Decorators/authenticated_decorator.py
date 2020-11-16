eaf896194effded857b9de51b7aecf8a265f335d
Elena Loghin <contact@elenaloghin.ro>
2020-11-16 17:26:43 +0200

    decorators

---
+++ PythonZTM/8. Decorators/authenticated_decorator.py
@@ -0,0 +1,20 @@
+# Create an @authenticated decorator that only allows the function to run is user1 has 'valid' set to True:
+user1 = {
+    'name': 'Sorna',
+    'valid': True #changing this will either run or not run the message_friends function.
+}
+
+def authenticated(fn):
+  # code here
+  def wrapper(*args, **kwargs):
+    if args[0]['valid'] == True:
+      fn(*args, **kwargs)
+    else:
+      print('message has not been sent')
+  return wrapper
+
+@authenticated
+def message_friends(user):
+    print('message has been sent')
+
+message_friends(user1)
